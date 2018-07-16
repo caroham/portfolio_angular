65d7cef6f6f9032b3dd236b9d4770f2a1c88baa6
Carolyn Hampe <carolyn.hampe@gmail.com>
2018-07-15 23:57:48 -0700

    test

--- client/src/app/hero.service.spec.ts
+++ client/src/app/hero.service.spec.ts
@@ -1,15 +1,15 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { HeroService } from './hero.service';
+import { Hero } from './hero.service';
 
 describe('HeroService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HeroService]
+      providers: [Hero]
     });
   });
 
-  it('should be created', inject([HeroService], (service: HeroService) => {
+  it('should be created', inject([Hero], (service: Hero) => {
     expect(service).toBeTruthy();
   }));
 });
